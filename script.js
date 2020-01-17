84df312579269b7c2d89dcf7e066ae19fbc583d6
Alex <dizzikz@gmail.com>
2020-01-17 12:13:25 +0300

    This is the first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second =2;
+const pi = 3.14;
+let string = "Hello!";
+var object = {};
+
+// console.log(typeof(second));
+
+// console.log(object);
+
+// console.log(string);
+// console.log(second + pi);
+
+// let persone = {
+//     name: "Jone",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone);
+// console.log(persone.name);
+// console.log(persone['name']);
+
+// let arr = ['1.jpg', '2.bmp', '3.png'];
+
+// console.log(arr[0], arr[2]);
+// console.log(typeof(persone.isMarried));
+// console.log(typeof(arr));
+
+// // alert(typeof(persone.name));
+// // let answer = confirm("Are you here?");
+
+// // console.log(answer);
+
+// let answer = +prompt("How old are you", "18");
+// // console.log(answer);
+// // alert("You are " + answer + "!");
+// console.log(typeof(answer));
+
+// let incr = 10,
+//     decr = 10;
+
+
+// console.log(incr++, decr--);
+// console.log(decr%3);
+
+// console.log(5 == "5");
+// console.log(5 === "5");
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+
+console.log(3 < 2 === 0);
